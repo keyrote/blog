7c9b8be12948272bb8f8364aa5373dca635d34fe
keyrote <keyrote@qq.com>
2022-12-12 21:31:25 +0800

    feat: 配置tailwindcss

--- .umirc.ts
+++ .umirc.ts
@@ -9,22 +9,34 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
+
   routes: [
     {
       path: '/',
       redirect: '/home',
     },
+
     {
       name: '首页',
       path: '/home',
       component: './Home',
     },
+
     { name: '创建博客', path: '/posts/create', component: 'posts/create' },
     { path: '/login', component: 'login' },
     { path: '/posts/:postId', component: 'posts/post' },
   ],
+
   apiRoute: {
     platform: 'vercel',
   },
+
+  extraPostCSSPlugins: [
+    require('tailwindcss')({
+      config: './tailwind.config.js',
+    }),
+  ],
+
   npmClient: 'pnpm',
+  tailwindcss: {},
 });
